cf78da4d9b59b73670706932ccab4ba2b0e95882
MartinJurkovic <martin.jurkovic94@gmail.com>
2022-07-22 12:51:32 +0200

    update

--- src/components/Country.js
+++ src/components/Country.js
@@ -4,11 +4,19 @@ import {Link} from "react-router-dom";
 export default function Country(props) {
 
     const appContext = useApp()
-    const {lightModeActive} = appContext
+    const {lightModeActive, width} = appContext
 
     const styles = {
         backgroundColor: lightModeActive ? "white" : "#3C4F63",
         color: lightModeActive ? "black" : "white"
+    }
+
+    const h2Styles = {
+        fontSize: width < 650 ? "18px" : "20px"
+    }
+
+    const h4Styles = {
+        fontSize: width < 650 ? "14px" : "16px"
     }
 
     return(
@@ -20,10 +28,10 @@ export default function Country(props) {
             
             <img className="country-img" src={props.e.flags.png}/>
             <div className="country-data">
-                <h2>{props.e.name.common}</h2>
-                <h4>Population: {props.e.population.toLocaleString('en-US')}</h4>
-                <h4>Region: {props.e.region}</h4>
-                <h4>{props.e.capital ? `Capital city: ${props.e.capital[0]}` : "No capital city"}</h4> 
+                <h2 style={h2Styles}>{props.e.name.common}</h2>
+                    <h4 style={h4Styles}>Population: {props.e.population.toLocaleString('en-US')}</h4>
+                    <h4 style={h4Styles}>Region: {props.e.region}</h4>
+                    <h4 style={h4Styles}>{props.e.capital ? `Capital city: ${props.e.capital[0]}` : "No capital city"}</h4>  
             </div>
             <h3></h3>
         </div>
